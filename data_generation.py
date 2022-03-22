87438ebeb7560c9e46cd0c0f3a7c465492e2216e
Daniel Joinson <ta20395@bristol.ac.uk>
2022-03-22 14:48:02 +0000

    Fixing errors in csv input

--- data_generation.py
+++ data_generation.py
@@ -15,11 +15,11 @@ case_data = pd.read_csv(f'{file_location}/Cumulative Cases.csv')
 
 case_data
 
-case_data = pd.read_csv("f'{file_location}/Cumulative Cases.csv")
+case_data = pd.read_csv(f'{file_location}/Cumulative Cases.csv')
 
 ## ADDING COVID-19 DEATH AND VACCINATION DATA
 
-vax_death_data = pd.read_csv("f'{file_location}/Vaccination and Death.csv")
+vax_death_data = pd.read_csv(f'{file_location}/Vaccination and Death.csv')
 
 ## MERGING CASE, DEATH AND VACCINATION DATAFRAMES TOGETHER
 
@@ -27,37 +27,37 @@ master_data = case_data.merge(vax_death_data, left_on='areaName', right_on='area
 
 ## ADDING AND MERGING AGE AND DENSITY DATA
 
-demo_data = pd.read_csv("f'{file_location}/Age and Density.csv")
+demo_data = pd.read_csv(f'{file_location}/Age and Density.csv')
 
 master_data = master_data.merge(demo_data, left_on='areaCode_x', right_on='Area code')
 
 ## ADDING AND MERGING ETHNICITY DATA
 
-eth_data = pd.read_csv("f'{file_location}/Ethnicity.csv")
+eth_data = pd.read_csv('f'{file_location}/Ethnicity.csv')
 
 master_data = master_data.merge(eth_data, left_on='areaCode_x', right_on='Area code')
 
 ## ADDING AND MERGING KEY WORKER DATA
 
-key_data = pd.read_csv("f'{file_location}/Key Worker.csv")
+key_data = pd.read_csv('f'{file_location}/Key Worker.csv')
 
 master_data = master_data.merge(key_data, left_on='areaCode_x', right_on='Area code')
 
 ## ADDING AND MERGING EMPLOYMENT DATA
 
-employ_data = pd.read_csv("f'{file_location}/Employment.csv")
+employ_data = pd.read_csv('f'{file_location}/Employment.csv')
 
 master_data = master_data.merge(employ_data, left_on='areaCode_x', right_on='Area code')
 
 ## ADDING AND MERGING CHILD DATA
 
-child_data = pd.read_csv("f'{file_location}/Children.csv")
+child_data = pd.read_csv('f'{file_location}/Children.csv')
 
 master_data = master_data.merge(child_data, left_on='areaCode_x', right_on='Area code')
 
 ## ADDING AND MERGING DEPRIVATION DATA
 
-depri_data = pd.read_csv("f'{file_location}/Deprivation.csv")
+depri_data = pd.read_csv('f'{file_location}/Deprivation.csv')
 
 master_data = master_data.merge(depri_data, left_on='areaCode_x', right_on='Area Code')
 
@@ -123,4 +123,4 @@ master_data["Deaths (cumulative)"] = master_data["Deaths (cumulative)"].astype(f
 
 save_location = input("Where would you like to save the data?")
 
-master_data.to_excel(f'{save_location}\Master Data.xlsx', index = False)
+master_data.to_excel(f'{save_location}\Master Data.xlsx', index = False')
