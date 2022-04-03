786c1df64d30bc5b08a7e8f2acc9390327808585
Tim Arueyingho <92372663+TheRealSheBoss@users.noreply.github.com>
2022-04-03 18:41:49 +0100

    Update ai_models.py

--- ai_models.py
+++ ai_models.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr  3 15:02:25 2022
-
-@author: danieljoinson
-"""
-
 def model_choice(file_location):
 
     ai_type = input("Do you want to use a regression model (R) or clustering model (C)? ")
