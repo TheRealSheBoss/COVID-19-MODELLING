9ecf91a727ed9155e98d229cf5ef7874471dcfc9
Tim_Arueyingho <tim.arueyingho@bristol.ac.uk>
2022-04-11 17:20:30 +0100

    Updated simulation in main.py

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ from data_generation import data_generation
 from info import info
 from ai_models import model_choice
 from ExploratoryDataAnalysis import eda
+from simulation import Main_function
 
 file_location = input("Where have you saved the non-master data? ")
 
 data_generation(file_location)
 
-function = ""
+function = ''
 
 
 while function != "stop":
-    print("Type G for graph function, I for variable information, A for AI modelling, E for eda or type stop to quit the program")
+    print("Type G for graph function, I for variable information, A for AI modelling, E for eda, S for simulation, "
+          "or type stop to quit the program")
     function = input("What do you want to do? ")
     print(f'Input was {function}')
     if function == "G":
@@ -29,10 +31,13 @@ while function != "stop":
     if function == "E":
         eda(file_location)
         continue
+    if function == "S":
+        Main_function()
+        continue
     if function == "stop":
         print("Stopping")
         break
     else:
         print("Invalid input")
         continue
-    
+
