f5922c3f45bf862b31eb0b179ccc683c5d28eb1b
kemond1 <ke13248@bristol.ac.uk>
2022-04-12 14:12:08 +0100

    Changed class to reference self.x rather than master_data. Changed input data for some of the graph functions to exclude local authority and region columns

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -28,59 +28,71 @@ def eda(file_location):
             #PROFILE.to_file(output_file="Master Data.html")
 
         def histplot_master_data(self):
-            for all, column in enumerate(master_data.columns):
+            for all, column in enumerate(self.x.columns):
                 plt.subplot(4, 6, all + 1)
-                sea.histplot(data=master_data[column])
+                #plt.tight_layout()
+                sea.histplot(data=self.x[column])
+                plt.xticks(rotation=30, fontsize=6)
                 plt.title(column)
-            plt.savefig('Histograms of all variables.png')
+            plt.savefig('Histograms of all variables.pdf')
             plt.tight_layout()
             plt.show()
 
         def lineplot_master_data(self):
-            for all, column in enumerate(master_data.columns):
+            for all, column in enumerate(self.x.columns):
                 plt.subplot(4, 6, all + 1)
-                sea.lineplot(data=master_data[column])
+                #plt.tight_layout()
+                plt.xticks(rotation=30, fontsize=6)
+                sea.lineplot(data=self.x[column])
                 plt.title(column)
 
-            plt.savefig('Lineplots of all variables.png')
+            plt.savefig('Lineplots of all variables.pdf')
             plt.tight_layout()
             plt.show()
 
         def scatterplot_master_data(self):
-            for all, column in enumerate(master_data.columns):
+            for all, column in enumerate(self.x.columns):
                 plt.subplot(4, 6, all + 1)
-                sea.scatterplot(data=master_data[column])
+                #plt.tight_layout()
+                plt.xticks(rotation=30, fontsize=6)
+                sea.scatterplot(data=self.x[column])
                 plt.title(column)
 
-            plt.savefig('Scatterplots of all variables.png')
+            plt.savefig('Scatterplots of all variables.pdf')
             plt.tight_layout()
             plt.show()
 
         def heatmap_master_data(self):
-            correlation = master_data.corr()
+            correlation = self.x.corr()
             fig, ax = plt.subplots(figsize=(10, 10))
             sea.heatmap(data=correlation, annot=False, cmap="seismic", center=0, linewidths=0.9)
-            plt.savefig('Master Data.png')
+            plt.savefig('Master Data.pdf')
             plt.show()
 
         def barchart_master_data(self):
-            for all, column in enumerate(master_data.columns):
+            for all, column in enumerate(self.x.columns):
                 plt.subplot(4, 6, all + 1)
-                sea.barplot(data=master_data[column])
+                #plt.tight_layout()
+                plt.xticks(rotation=30, fontsize=6)
+                sea.barplot(data=self.x[column])
                 plt.title(column)
 
-            plt.savefig('Barplots of all variables.png')
+            plt.savefig('Barplots of all variables.pdf')
             plt.tight_layout()
             plt.show()  
 
     View_charts = ExploreMasterData(master_data)
-    #View_charts.master_data_profile()
-    View_charts.histplot_master_data()
-    View_charts.lineplot_master_data()
-    View_charts.scatterplot_master_data()
     View_charts.heatmap_master_data()
-    View_charts.barchart_master_data()
-
+    master_data_copy = master_data.copy()
+    master_data_copy = master_dasta_copy.drop(columns=['Local Authority', 'Region name'])
+    View_charts2 = ExploreMasterData(master_data_copy)
+    #View_charts.master_data_profile()
+    View_charts2.histplot_master_data()
+    #View_charts2.lineplot_master_data()
+    #View_charts2.scatterplot_master_data()
+    #View_charts2.barchart_master_data()
+    
+    
     
 
 
