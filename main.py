54a4790651c5acba9375bfc33cf207d5a486e9f0
JoinsonCoding <94851738+JoinsonCoding@users.noreply.github.com>
2022-04-21 12:42:30 +0100

    adding docstrings

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ from simulation import Main_function
 from ExploratoryDataAnalysis import eda_selection
 from ExploratoryDataAnalysis import ExploreMasterData
 
+"""
+This program prompts the user to input the location they saved the relevant files - and then the data_generation program 
+is used to generate the master data. After this, a while loop is used to generate an interface where the user choices which
+function of the program to use. This loop breaks only if the user inputs stop. 
+
+Each file which the user can select to use contains a function which file_location is passed to. This means that these files 
+can have access to master_data, without the user having to input the file location repeatedly.
+"""
+
 file_location = input("Where have you saved the non-master data? ")
 
 data_generation(file_location)
