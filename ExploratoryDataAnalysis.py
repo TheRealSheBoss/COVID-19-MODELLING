e2d31d4033ee82771351310a9d0d823395279ebf
kemond1 <ke13248@bristol.ac.uk>
2022-04-26 13:22:09 +0100

    removing unneeded commented lines of code

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -13,8 +13,7 @@ def eda_selection(file_location):
     sea.set(font_scale=0.6)
     import pandas as pd
     master_data = pd.read_csv(f'{file_location}/Master Data.csv')
-    # % matplotlib inline  (uncomment if using Jupyter NB)
-    #plt.figure(figsize=(25, 15))
+    # % matplotlib inline  #(uncomment if using Jupyter NB)
     master_data_copy = master_data.copy()
     master_data_copy = master_data_copy.drop(columns=['Region name', 'Local Authority'])
     View_charts2 = ExploreMasterData(master_data_copy)
@@ -29,11 +28,8 @@ class ExploreMasterData:
         self.x = x
 
     def histplot_master_data(self):
-        #for all, column in enumerate(master_data.columns):
         for all, column in enumerate(self.x.columns):
             plt.subplot(5, 4, all + 1)
-            #sea.histplot(data=master_data[column])
-            #plt.tight_layout()
             sea.histplot(data=self.x[column], palette='husl')
             plt.xticks(rotation=30,fontsize=5)
             plt.yticks(rotation=30,fontsize=5)
@@ -55,7 +51,6 @@ class ExploreMasterData:
         plt.show()
 
     def lineplot_master_data(self):
-        #for all, column in enumerate(master_data.columns):
         for all, column in enumerate(self.x.columns):
             plt.subplot(4, 6, all + 1)
             sea.lineplot(data=self.x[column])
@@ -79,7 +74,6 @@ class ExploreMasterData:
         plt.show()
 
     def scatterplot_master_data(self):
-        #for all, column in enumerate(master_data.columns):
         for all, column in enumerate(self.x.columns):
             plt.subplot(4, 6, all + 1)
             sea.scatterplot(data=self.x[column])
@@ -103,7 +97,6 @@ class ExploreMasterData:
         plt.show()
 
     def heatmap_master_data(self):
-        #correlation = master_data.corr()
         correlation = self.x.corr()
         fig, ax = plt.subplots(figsize=(10, 10))
         sea.heatmap(data=correlation, annot=False, cmap="seismic", center=0, linewidths=0.9)
@@ -116,10 +109,8 @@ class ExploreMasterData:
         plt.show()
 
     def boxplot_master_data(self):
-        #for all, column in enumerate(master_data.columns):
         for all, column in enumerate(self.x.columns):
             plt.subplot(5, 4, all + 1)
-            # plt.tight_layout()
             plt.xticks(rotation=30, fontsize=5)
             plt.yticks(rotation=30,fontsize=5)
             box_plt = sea.boxplot(data=self.x[column], linewidth = 0.8, width = 0.6, showfliers = False)
